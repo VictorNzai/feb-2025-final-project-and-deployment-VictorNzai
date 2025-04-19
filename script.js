b150d5cabc7718beb8d065751c0888095355f7e5
VictorNzai <victor.nzai@strathmore.edu>
2025-04-19 20:57:16 +0300

    Script File

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {  
+    const readMoreLinks = document.querySelectorAll('a[href$=".html"]'); //Selects all elements with attribute a and .html  
+    readMoreLinks.forEach(link => {  
+        link.addEventListener('click', function(event) {  
+ 
+        });  
+    });  
+});  
